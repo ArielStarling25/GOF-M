b8e88c386c19468e9316e5062bc27c6977afe64c
Ariel Starling <arielstarling25@gmail.com>
2026-01-21 16:31:27 +0800

    Changes - 21/01/26 - 2 - Managed to get custom data generation to work

--- custom_data_gen.py
+++ custom_data_gen.py
@@ -1,14 +1,24 @@
 # training scripts for the TNT datasets
 import os
-import GPUtil
 from concurrent.futures import ThreadPoolExecutor
 import time
 from pathlib import Path
 
-dry_run = False
+# NOTE: Current implementation is for windows environment, not linux
+
+DRY_RUN = False
+
+# INPUT_DIR = os.path.join("custom_datasets", "Panther")
+INPUT_DIR = "D:/Masters-Alt/GOF-M/custom_datasets/Panther"
+
+COLMAP_DIR = "D:/Masters-Alt/COLMAP/COLMAP.bat"
+
+MAGICK_DIR = "D:/Masters-Alt/ImageMagick/magick.exe"
 
 def run_script():
-    cmd = f"python3 convert.py "
+    cmd = f"python convert.py -s {INPUT_DIR} --colmap_executable {COLMAP_DIR} --magick_executable {MAGICK_DIR} --resize"
     print(cmd)
-    if not dry_run:
+    if not DRY_RUN:
         os.system(cmd)
+
+run_script()
